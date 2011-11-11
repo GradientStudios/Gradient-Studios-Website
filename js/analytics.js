f3ac82e19cc239aba69368cf26ee027ba6d25559
Trevor Sayre <sayret@gmail.com>
2011-11-11 17:11:54 -0500

    modified aboutus to fit mobile, modified index to better fit mobile, modified analytics to ignore my local tests

--- js/analytics.js
+++ js/analytics.js
@@ -1,22 +1,14 @@
-var account = '';
-var hostname = window.location.host;
-switch(hostname) {
-  case 'www.gradient-studios.com':
-    account = 'UA-20856190-1'; 
-    break;
-  case 'www.gradientstudios.com':
-    account = 'UA-20856190-7'; 
-    break;
-  default: account = 'UA-20856190-1';
+if(window.location.host != 'alecto.local') {
+  
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-20856190-7']);
+  _gaq.push(['_setDomainName', '.gradientstudios.com']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
 }
-
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', account]);
-_gaq.push(['_setDomainName', '.' + hostname]);
-_gaq.push(['_trackPageview']);
-
-(function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
