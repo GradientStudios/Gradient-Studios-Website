672309ae5f7b72c501b10b9fdaedd79184fb8e5d
Trevor Sayre <sayret@gmail.com>
2011-11-11 17:26:53 -0500

    fixed aboutus.js, removed alert

--- js/aboutus.js
+++ js/aboutus.js
@@ -3,7 +3,6 @@ $(document).ready(function() {
   var textBlock = '';
   $('span.text,div#companyBio span').each(function() {
     textBlock = $(this).html().replace(/ ([^\-\s]+)$/,'&nbsp;$1');
-    alert(textBlock);
     $(this).html(textBlock);
   });
   
