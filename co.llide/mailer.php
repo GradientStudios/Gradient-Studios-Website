0ff8ef3c7ddea8c3137c74fab5b682dfa4f750ab
Trevor Sayre <sayret@gmail.com>
2013-06-21 17:41:55 -0400

    Change location for success.html to full path

--- co.llide/mailer.php
+++ co.llide/mailer.php
@@ -9,6 +9,6 @@ $text = "Name: $name $lname <br>
 $headers = "MIME-Version: 1.0" . "\r\n"; 
 $headers .= "Content-type:text/html; charset=utf-8" . "\r\n"; 
 $headers .= "From: <$email>" . "\r\n";
-header("Location: success.html");
+header("Location: http://www.gradientstudios.com/co.llide/success.html");
 mail($email_to, $emailSubject, $text, $headers);
 ?>
